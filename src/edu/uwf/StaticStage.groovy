64f858d446d3c17069df1ed69a7aad0697d5da8a
Nathan Cooper <nac33@students.uwf.edu>
2017-02-11 18:18:37 -0600

    Trying without steps.(dot) to do a docker build

--- src/edu/uwf/StaticStage.groovy
+++ src/edu/uwf/StaticStage.groovy
@@ -21,7 +21,7 @@ class StaticStage implements Serializable
             steps.dir("${path}")
             {
 
-                staticImage = steps.docker.build("${repo}")
+                staticImage = docker.build("${repo}")
                 //steps.sh "docker build -t ${repo} ${path}"
                 //sh "docker run -v /home/ec2-user/workspace/jenkins_pipeline/${repo}:/pipeline --rm ${repo}"
 
