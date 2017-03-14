177482e47400e39323b786c98ede003fb8508f7d
Nathan Cooper <Nathan Cooper>
2017-03-14 12:32:06 -0500

    Debugging

--- vars/stage.groovy
+++ vars/stage.groovy
@@ -1,10 +1,3 @@
-// vars/sayHello.groovy
-def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    echo "Hello, ${name}."
-}
-
 def createEnvironment(repo, path, stage)
 {
 
