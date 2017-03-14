08aed145643155d51827db4018d377d03a432824
Nathan Cooper <Nathan Cooper>
2017-03-13 20:43:42 -0500

    Add integration test to pipeline

--- src/edu/uwf/BuildStage.groovy
+++ src/edu/uwf/BuildStage.groovy
@@ -18,12 +18,12 @@ class BuildStage implements Serializable
         {
 
             // Building the docker image from the Dockerfile
-            steps.sh "docker build -t jpipeline ${path}"
-            steps.sh "docker run -t -d -v /home/ec2-user/workspace/jenkins_pipeline/${repo}:/pipeline --name \"jpipeline\" jpipeline"
-            steps.sh "docker exec jpipeline bash -c 'cd /maven/${repo}/MediumFX/;mvn compile'"
+            steps.sh "docker build -t buildimage ${path}"
+            steps.sh "docker run -t -d -v /home/ec2-user/workspace/jenkins_pipeline/${repo}:/maven --name \"build\" buildimage"
+            steps.sh "docker exec build bash -c 'cd /maven/${repo}/MediumFX/;mvn compile'"
 
-            steps.sh "docker stop jpipeline"
-            steps.sh "docker rm jpipeline"
+            steps.sh "docker stop build"
+            steps.sh "docker rm buildimage"
 
         }
 
