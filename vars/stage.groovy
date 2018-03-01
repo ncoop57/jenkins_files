53c9bdc296b6f400a292006522e5d3e96efac1e6
Nathan Cooper <nac33@students.uwf.edu>
2018-03-01 20:37:12 +0000

    Testing

--- vars/stage.groovy
+++ vars/stage.groovy
@@ -12,11 +12,40 @@ def createEnvironment(repo, url, branch, path, stage)
       // Perform the action required for the staging environment
       dir("/cdep/repos/$repo")
       {
+        sh "docker exec -i staging bash /var/www/html/staging.sh ${url} /var/www/html/${repo} $branch"
+      }
+
+    }
+    else if (stage.equals("build")
+    {
+
+      // Build the docker image for the specific stage using the Dockerfile in
+      // the current directory
+      def image = docker.build("${stage}");
+      withEnv(["REPO=${repo}", "BRANCH=${branch}"]) // Pass the repo name as an environment variable
+      {
+
+        // Run the docker container with the specified default network and
+        // mounting folder
+        image.inside("--network=dockercompose_default -v /cdep:/cdep")
+        {
+
+          // Execute the specific build script for the given stage on the repo
+          sh "bash build.sh $REPO $BRANCH"
+
+        }
+
+      }
+
+      dir("/cdep/repos/$repo")
+      {
+        sh "git add -A"
+        sh "git commit -m \"Successfully built application.jar\""
+        sh "git push origin $branch"
         sh "git checkout master"
         sh "git pull origin master"
         sh "git merge $branch"
         sh "git push origin master"
-        sh "docker exec -i staging bash /var/www/html/staging.sh ${url} /var/www/html/${repo} $branch"
       }
 
     }
@@ -26,7 +55,7 @@ def createEnvironment(repo, url, branch, path, stage)
       // Build the docker image for the specific stage using the Dockerfile in
       // the current directory
       def image = docker.build("${stage}");
-      withEnv(["REPO=${repo}"]) // Pass the repo name as an environment variable
+      withEnv(["REPO=${repo}", "BRANCH=${branch}"]) // Pass the repo name as an environment variable
       {
 
         // Run the docker container with the specified default network and
@@ -35,7 +64,7 @@ def createEnvironment(repo, url, branch, path, stage)
         {
 
           // Execute the specific build script for the given stage on the repo
-          sh "bash build.sh $REPO"
+          sh "bash build.sh $REPO $BRANCH"
 
         }
 
